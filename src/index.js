8cef7e374f5b09e30d4de81e851e6e5cc5b16fd4
tessallen-cmd <tessstablet@gmail.com>
2025-06-09 16:31:31 +0100

    add api intergration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  // goal is to make api call and update UI user interface
+  let apiKey = "9117d16f27ad34748062df20bto34069";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+  //seperation of concern  means it will only do a job that is assigned
+  //ask axios to update data for new city
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 
   //next task call API
   //search for the city
@@ -10,3 +29,6 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
+//default when loading page city name and temperature now not ard coded into the html
