e2d6686978e73194bce6872d2ce77b98f405b07e
tessallen-cmd <tessstablet@gmail.com>
2025-06-09 18:39:57 +0100

    inject weather data

--- src/index.js
+++ src/index.js
@@ -3,9 +3,42 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
 
+  let date = new Date(response.data.time * 1000);
+
+  
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = ` ${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minute = `0${minutes}`;
+  }
+
+  return `${day} ${hours}: ${minutes}`;
 }
 
 function searchCity(city) {
@@ -31,4 +64,4 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
-//default when loading page city name and temperature now not ard coded into the html
+//default when loading page city name and temperature now not hard coded into the html
